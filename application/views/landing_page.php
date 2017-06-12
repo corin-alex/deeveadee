b9c883289104868c9b62af6e99ae9e49fa69989b
Corin Alexandru <corin.alex@gmail.com>
2017-06-13 01:27:12 +0200

    maps

--- application/views/landing_page.php
+++ application/views/landing_page.php
@@ -1,6 +1,4 @@
-<h2 class="text-center">
-    Nouveautés
-</h2>
+<h2 class="text-center">Nouveautés</h2>
 <div class="container-fluid">
     <div class="row">
     <?php foreach ($last_dvds as $dvd): ?>
@@ -24,3 +22,27 @@
     <a href="<?= site_url('dvd/list/'); ?>">Voir tous les DVD</a>
     <br><br>
 </div>
+
+<h2 class="text-center">Nos boutiques</h2>
+<div id="map"></div>
+<script>
+    function initMap() {
+        var uluru = {lat: 43.605160, lng: 3.882201};
+        var uluruu = {lat: 43.346642, lng: 3.221606};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 20,
+            center: uluru
+        });
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
+        });
+        var marker2 = new google.maps.Marker({
+            position: uluruu,
+            map: map
+        });
+    }
+</script>
+<script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB9GMpu6HgmVgNitPurY_CWM07BY9l5QiA &callback=initMap">
+</script>
